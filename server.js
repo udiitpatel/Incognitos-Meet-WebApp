3e16c222e99b39cb365171df427f935212d5c2f0
prahlad pawar <prahladpawar@prahlads-MacBook-Air.local>
2022-09-03 22:52:15 +0530

    version 1.1 Incognitos

--- server.js
+++ server.js
@@ -2,17 +2,26 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const { v4: uuidv4 } = require('uuid');
-
+const io = require('socket.io')(server);
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
+app.use('/peerjs', peerServer);
 app.get('/', (req, res) => {
     res.redirect(`/${uuidv4()}`);
 })
 app.get('/:room', (req, res) => {
      res.render('room',{roomId: req.params.room})
 })
+io.on('connection', socket => {
+    socket.on('join-room', (roomId,userId) => {
+        socket.join(roomId);
+        socket.broadcast.to(roomId).emit('user-connected',userId);
+    })
+})
 
-
-server.listen(3030)
+server.listen(3040)
 
